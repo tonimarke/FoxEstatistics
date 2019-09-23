5b88ffdf79ddcac94780671127321df7e0b0f6bd
xmasui <arthur.matugi@gmail.com>
2019-09-23 08:09:27 -0300

    Update estat.js

--- estat.js
+++ estat.js
@@ -1,4 +1,4 @@
-let input = [], m, desvP, array = [], val = 0, resp = 0, Fi;
+let input = [], m, desvP, array = [], val = 0, resp = 0, Fi, freq = [], elemento = [], objTabela = [];
 
 //Criará o ROL
 function criarRol(input, array){
@@ -18,21 +18,21 @@ function criarRol(input, array){
 				}
 				array[j + 1] = el;
 			}
-		return array;
 	}
 	else if (document.getElementById('QN').checked || document.getElementById('QO').checked){
 		for (i = 0; i < aux.length; i++){
 			array.push(aux[i]);
 		}
 		array.sort();
-		return array;
 	}
 	moda(array);
+	media(array, elemento, freq);
+	criarTab(elemento, array, freq, objTabela);
 }
 
 //MODA
 function moda(array){
-	var i, freq = [], elemento = [], contagem = 0, aux = array[0], fAux = 1, m = "";
+	var i, contagem = 0, aux = array[0], fAux = 1, m = "";
 
 	for(i = 1; i < array.length; i++){
 		if(aux == array[i]){
@@ -68,14 +68,40 @@ function moda(array){
 	//falta verificar para amodal  
 }
 
-/*MÉDIA
-function media(array){
+//MÉDIA
+function media(array, elemento, freq){
+	var soma = 0;
 	if (document.getElementById('QD').checked || document.getElementById('QC').checked){
-		
+		for (i = 0; i < elemento.length; i++){
+			soma += elemento[i] * freq[i];
+		}
+
 	}
+	console.log(soma/array.length);
 }
 
-//Intevalo de classes
+/*//MEDIANA
+function mediana(array){
+	var mediana;
+	var p1;
+	if (document.getElementById('QD').checked){
+		if(array.length % 2 == 0){
+			p1 = array.length/2;
+			var p2 = array.length/2+1;
+			mediana = (parseFloat(array[p1-1]) + parseFloat(array[p2-1]))/2;
+		}
+			else{
+				p1 = (array.length+1)/2;
+				mediana = parseFloat(array[p1-1]);
+			}
+	}
+	else if(document.getElementById('QC').checked){
+
+	}
+return mediana;
+}*/
+
+/*//Intevalo de classes
 function iClasses(array, Fi){
 	var Fi = array.length;
 	var AA = array[Fi-1] - array[0] + 1;
@@ -92,27 +118,44 @@ function iClasses(array, Fi){
 		console.log(Math.ceil(h));
 		return Math.ceil(h);
 	}
-}
+}*/
 
-//MEDIANA
-function mediana(array){
-	var mediana;
-	var p1;
-	if(array.length % 2 == 0){
-		p1 = array.length/2;
-		var p2 = array.length/2+1;
-		mediana = (parseFloat(array[p1-1]) + parseFloat(array[p2-1]))/2;
+//CRIA TABELA
+function criarTab(elemento, array, freq, objTabela){
+	var varPesquisadas = [], fSimples = [], fRelativa = [], fAcumulada = [];
+	var total = 0, frPercem = [], teste = [3, 5, 4, 7];
+
+	for (i = 0; i < elemento.length; i++){
+		total += elemento[i];
 	}
-        else{
-            p1 = (array.length+1)/2;
-            mediana = parseFloat(array[p1-1]);
-        }
-return mediana;
+
+		if (document.getElementById('QN').checked || document.getElementById('QO').checked){
+			objTabela.varPesquisadas = elemento;
+			objTabela.fSimples = freq;
+
+			for (i = 0; i < freq.length; i++){
+
+				frPercem.push(teste[i]/total);
+			}
+
+			objTabela.PfrPercemsvmkl = frPercem;
+			console.table(objTabela);
+				/*objTabela.push({
+					varPesquisadas: elemento[i];
+					fSimples: freq[i];
+					fRelativa: freq[i]/total;
+					fAcumulada: soma += freq[i];
+				});
+				objTabela.varPesquisadas.push(elemento[i]);
+				objTabela.fSimples.push(freq[i]);
+				objTabela.fRelativa.push(freq[i] / total);
+				objTabela.fAcumulada.push(soma += freq[i]);*/
+		}
 }
 
-//VARIÂNCIA
+/*//VARIÂNCIA
 function variancia(array, val){
-	if (getElementById('p').checked){
+	if (document.getElementById('p').checked){
 		for(var i = 0; i < array.length; i++){
 			resp += Math.pow((parseFloat(array[i]) - val), 2);
 		}
